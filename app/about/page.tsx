807c671eb82dc2bb1dc6a0ec70f5a9c54039eef5
ccozens <ccozens@gmail.com>
2023-06-05 14:43:46 +0100

    removed home boilerplate, added about page, changed background gradient in Layout.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+// `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
+export default function Page() {
+  return (
+    <>
+      <h1 className="text-gray-800 align-middle">Hello, Dashboard About!</h1>
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,17 +7,10 @@ import Footer from "@/components/layout/footer";
 import { Suspense } from "react";
 
 export const metadata = {
-  title: "Precedent - Building blocks for your Next.js project",
-  description:
-    "Precedent is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
-  twitter: {
-    card: "summary_large_image",
-    title: "Precedent - Building blocks for your Next.js project",
-    description:
-      "Precedent is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
-    creator: "@steventey",
-  },
-  metadataBase: new URL("https://precedent.dev"),
+  title: "shortcuts-wiki",
+  description: "Personal wiki, based on Precedent.js",
+
+  // metadataBase: new URL("https://precedent.dev"),
   themeColor: "#FFF",
 };
 
@@ -29,16 +22,17 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={cx(sfPro.variable, inter.variable)}>
-        <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
-        <Suspense fallback="...">
-          {/* @ts-expect-error Server Component */}
-          <Nav />
-        </Suspense>
-        <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
-          {children}
-        </main>
-        <Footer />
-        <Analytics />
+        <div className="fixed h-screen w-full bg-gradient-to-b from-black to-green-700">
+          <Suspense fallback="...">
+            {/* @ts-expect-error Server Component */}
+            <Nav />
+          </Suspense>
+          <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
+            {children}
+          </main>
+          <Footer />
+          <Analytics />
+        </div>
       </body>
     </html>
   );
