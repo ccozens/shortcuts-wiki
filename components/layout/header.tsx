b361ad0926c24b2d671e99e65572acea2edb0bfb
ccozens <ccozens@gmail.com>
2023-06-08 22:59:17 +0100

    updated title

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,11 +1,13 @@
 import Nav from "./nav";
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <div className="mx-5 flex h-16 max-w-screen-xl justify-between">
       <h1 className="m-4 text-4xl font-extrabold leading-none tracking-tight  text-green-400 md:text-5xl lg:text-6xl ">
-        {" "}
-        All the things{" "}
+        <Link href="https://www.youtube.com/watch?v=9Ht5RZpzPqw" target="_blank">
+        All the small things
+        </Link>
       </h1>
       {/* @ts-expect-error Server Component */}
       <Nav />
